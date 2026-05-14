76e0b8984360df50ded3020c709983c590f8b769
Melvin Callisaya <mattct2393@gmail.com>
2026-05-14 15:05:22 -0400

    Task - Add dependencies, plugins, KSP and Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -58,10 +59,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-
+    
     implementation(libs.retrofit)
     implementation(libs.retrofit.kotlinx.serialization)
     implementation(libs.okhttp3)
     implementation(libs.serialization.json)
+
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
 }
