36b85886cd26ebbe59d54457166b3d659f005896
Melvin Callisaya <mattct2393@gmail.com>
2026-05-14 14:59:59 -0400

    Task - Implemented Retrofit and Kotlin Serialization

---
+++ app/src/main/java/dev/mcallisaya/techchallenge/data/network/ApiRest.kt
@@ -0,0 +1,26 @@
+package dev.mcallisaya.techchallenge.data.network
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dev.mcallisaya.techchallenge.util.baseUrl
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.create
+
+class ApiRest {
+    private fun createBuilder(): Retrofit.Builder {
+        val kotlinSerialization = Json {
+            isLenient = true
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+            encodeDefaults = true
+        }
+
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(kotlinSerialization.asConverterFactory("application/json".toMediaType()))
+            .baseUrl(baseUrl)
+    }
+
+    fun <T> getService(cls: Class<out T>): T = createBuilder().build().create()
+}
---
+++ app/src/main/java/dev/mcallisaya/techchallenge/util/Const.kt
@@ -0,0 +1,3 @@
+package dev.mcallisaya.techchallenge.util
+
+val baseUrl = "https://rickandmortyapi.com/api"
